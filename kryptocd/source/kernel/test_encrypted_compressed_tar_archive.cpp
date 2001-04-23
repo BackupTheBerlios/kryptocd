6a64ea5b031a7b8a702d437a1544dce11c288469
t-peters <t-peters>
2001-04-23 15:24:34 +0000

    Added comments

--- kryptocd/source/kernel/test_encrypted_compressed_tar_archive.cpp
+++ kryptocd/source/kernel/test_encrypted_compressed_tar_archive.cpp
@@ -1,5 +1,5 @@
 // test_encrypted_compressed_tar_archive.cpp
-// $Id: test_encrypted_compressed_tar_archive.cpp,v 1.2 2001/04/23 12:48:20 t-peters Exp $
+// $Id: test_encrypted_compressed_tar_archive.cpp,v 1.3 2001/04/23 15:24:34 t-peters Exp $
 
 #include "tar_creator.hh"
 #include "bz2_compressor.hh"
@@ -9,34 +9,34 @@
 
 int main(int argc, char ** argv)
 {
+    /* The archive file */
     int outputFd =
         open("/tmp/kryptocd_test.tar.bz2.gpg",
              O_WRONLY|O_CREAT|O_TRUNC,
              0644);
+
+    /* a list of files to put into the archive */
     std::list<std::string> files;
+    
     for (int i = 1; i < argc; ++i) {
         files.push_back(argv[i]);
     }
-
     KryptoCD::TarCreator * tar =
         new KryptoCD::TarCreator("/bin/tar",
                                  files);
-
     KryptoCD::Bz2Compressor * bz2 =
         new KryptoCD::Bz2Compressor("/usr/bin/bzip2",
                                     6, // compression rate
                                     tar->getStdoutPipeFd());
-
     KryptoCD::GpgEncrypter * gpg =
         new KryptoCD::GpgEncrypter("/usr/bin/gpg",
                                    "some_password",
                                    bz2->getStdoutPipeFd(),
                                    outputFd);
-
     tar->wait();
     delete tar;
     tar = 0;
-   
+
     bz2->wait();
     delete bz2;
     bz2 = 0;
