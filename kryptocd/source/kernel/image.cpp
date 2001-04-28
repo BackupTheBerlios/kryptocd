f84796452429b2b16967f8db31401ba414287677
t-peters <t-peters>
2001-04-28 11:30:33 +0000

    added implementation for testing purposes. Works.

--- kryptocd/source/kernel/image.cpp
+++ kryptocd/source/kernel/image.cpp
@@ -1,7 +1,7 @@
 /*
- * image.hh: class Image implementation
+ * image.cpp: class Image implementation
  * 
- * $Id: image.cpp,v 1.1 2001/04/25 14:26:52 t-peters Exp $
+ * $Id: image.cpp,v 1.2 2001/04/28 11:30:33 t-peters Exp $
  *
  * This file is part of KryptoCD
  * (c) 2001 Tobias Peters
@@ -24,11 +24,14 @@
 
 #include "image.hh"
 #include "tar_creator.hh"
+#include "tar_lister.hh"
 #include "bzip2.hh"
 #include "gpg.hh"
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
+
+#include <iostream>
 
 using KryptoCD::Image;
 using KryptoCD::TarCreator;
@@ -39,7 +42,12 @@ using KryptoCD::Diskspace;
 using std::string;
 using std::list;
 
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1024
+#endif
+
 Image::Image(const string & image_id_,
+             const string & password,
              const list<string> & files,
              Diskspace & diskspace_,
              int cdCapacity,
@@ -52,15 +60,125 @@ Image::Image(const string & image_id_,
       diskspace(diskspace_),
       allocatedMegabytes(0)
 {
-    allocatedMegabytes =diskspace.allocate(cdCapacity*2048.0/(1024.0*1024.0)+1);
+    char buffer[BUFFER_SIZE];
+    int targetFileSize = 0;
+    
+    // allocatedMegabytes =
+    //     diskspace.allocate(cdCapacity*2048.0/(1024.0*1024.0)+1);
 
-    TarCreator * tar = new TarCreator("/bin/tar",
-                                 files);
-    Bzip2 * bz2 = new Bzip2("/usr/bin/bzip2",
-                            6, // compression rate
-                            tar->getStdoutPipeFd());
-    Gpg * gpg = new Gpg("/usr/bin/gpg", "some_password",
-                        Gpg::ENCRYPT,
-                        bz2->getStdoutPipeFd());
+    TarCreator * tarCreator = new TarCreator(tarExecutable, files);
+    Bzip2 * bz2Compressor = new Bzip2(bzip2Executable, 6, // compression rate
+                                      tarCreator->getStdoutPipeFd());
+    Gpg * gpgEncrypter = new Gpg(gpgExecutable, password, Gpg::ENCRYPT,
+                                 bz2Compressor->getStdoutPipeFd());
 
-    creat(diskspace
+    Gpg * gpgDecrypter = new Gpg(gpgExecutable, password, Gpg::DECRYPT);
+
+    // the decrypter's stdin may not be shared by child processes:
+    setCloseOnExecFlag(gpgDecrypter->getStdinPipeFd());
+    Bzip2 * bz2Inflator = new Bzip2(bzip2Executable, -1, // -1 means decompress
+                                    gpgDecrypter->getStdoutPipeFd());
+    TarLister * tarLister = new TarLister(tarExecutable,
+                                          bz2Inflator->getStdoutPipeFd());
+
+    int intermediateFd =
+        open("/tmp/kryptocd_inter_test.tar.bz2.gpg",
+             O_WRONLY|O_CREAT|O_TRUNC,
+             0644);
+
+    do {
+        int bytesLeft = cdCapacity - targetFileSize;
+        int bytesThisTime;
+
+        bytesThisTime = read(gpgEncrypter->getStdoutPipeFd(), buffer,
+                             BUFFER_SIZE);
+
+        if (bytesThisTime == 0) {
+            // The whole archive fits on one cd
+            break;
+        }
+
+        assert(bytesThisTime > 0);
+
+        targetFileSize += bytesThisTime;
+
+        if (targetFileSize >= cdCapacity) {
+            bytesThisTime -= targetFileSize - cdCapacity;
+            assert (bytesThisTime >= 0);
+        }
+
+        write(intermediateFd, buffer, bytesThisTime);
+        
+        while (bytesThisTime > 0) {
+            int bytesWritten;
+            
+            bytesWritten = write(gpgDecrypter->getStdinPipeFd(), buffer,
+                                 bytesThisTime);
+
+            assert(bytesWritten > 0);
+            bytesThisTime -= bytesWritten;
+        }
+    } while (targetFileSize < cdCapacity);
+    close(gpgDecrypter->getStdinPipeFd());
+    close(intermediateFd);
+    
+    // kill the archive creating processes
+    delete tarCreator;
+    tarCreator = 0;
+    delete bz2Compressor;
+    bz2Compressor = 0;
+    delete gpgEncrypter;
+    gpgEncrypter = 0;
+
+    // wait for the archive listing processes to finish
+    gpgDecrypter->wait();
+    delete gpgDecrypter;
+    gpgDecrypter = 0;
+    bz2Inflator->wait();
+    delete bz2Inflator;
+    bz2Inflator = 0;
+
+    list<string> newFileList;
+    if (targetFileSize < cdCapacity) {
+        newFileList = files;
+    } else {
+        newFileList = tarLister->getFileList();
+        // the last file is probably incomplete
+        if (!newFileList.empty()) {
+            newFileList.pop_back();
+        }
+    }
+
+    for (std::list<std::string>::const_iterator i=newFileList.begin();
+         i != newFileList.end(); ++i) {
+        cout << "will include file: "<< *i << endl;
+    }
+
+    
+    
+    delete tarLister;
+    tarLister = 0;
+
+    /* The archive file */
+    int outputFd =
+        open("/tmp/kryptocd_test.tar.bz2.gpg",
+             O_WRONLY|O_CREAT|O_TRUNC,
+             0644);
+    
+    tarCreator = new TarCreator(tarExecutable, newFileList);
+    bz2Compressor = new Bzip2(bzip2Executable, 6, // compression rate
+                              tarCreator->getStdoutPipeFd());
+    gpgEncrypter = new Gpg(gpgExecutable, password, Gpg::ENCRYPT,
+                           bz2Compressor->getStdoutPipeFd(),
+                           outputFd);
+
+    tarCreator->wait();
+    delete tarCreator;
+    tarCreator = 0;
+    bz2Compressor->wait();
+    delete bz2Compressor;
+    bz2Compressor = 0;
+    gpgEncrypter->wait();    
+    delete gpgEncrypter;
+    gpgEncrypter = 0;
+}    
