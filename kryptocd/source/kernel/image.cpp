28305c1d92545bb6e0c50f19f7c00a7f5009b3ff
t-peters <t-peters>
2001-05-02 21:47:07 +0000

    Almost complete new implementation using higher level helper classes

--- kryptocd/source/kernel/image.cpp
+++ kryptocd/source/kernel/image.cpp
@@ -1,7 +1,7 @@
 /*
  * image.cpp: class Image implementation
  * 
- * $Id: image.cpp,v 1.2 2001/04/28 11:30:33 t-peters Exp $
+ * $Id: image.cpp,v 1.3 2001/05/02 21:47:07 t-peters Exp $
  *
  * This file is part of KryptoCD
  * (c) 2001 Tobias Peters
@@ -23,10 +23,9 @@
  */
 
 #include "image.hh"
-#include "tar_creator.hh"
-#include "tar_lister.hh"
-#include "bzip2.hh"
-#include "gpg.hh"
+#include "archive_creator.hh"
+#include "archive_lister.hh"
+#include "io_pump.hh"
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -34,151 +33,216 @@
 #include <iostream>
 
 using KryptoCD::Image;
-using KryptoCD::TarCreator;
-using KryptoCD::Bzip2;
-using KryptoCD::Gpg;
+using KryptoCD::ArchiveCreator;
+using KryptoCD::ArchiveLister;
 using KryptoCD::Diskspace;
 
 using std::string;
 using std::list;
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1024
-#endif
-
-Image::Image(const string & image_id_,
-             const string & password,
-             const list<string> & files,
+Image::Image(const string & imageId_,
+             const string & password_,
+             int compression_,
+             list<string> & files_,
+             list<string> & reject_,
+             list<ImageInfo> & imageInfos,
              Diskspace & diskspace_,
-             int cdCapacity,
-             Method method,
-             const string & tarExecutable,
-             const string & bzip2Executable,
-             const string & gpgExecutable,
-             const string & mkisofsExecutable)
-    : image_id(image_id_),
+             int cdCapacity_,
+             Image::Method method_,
+             const string & tarExecutable_,
+             const string & bzip2Executable_,
+             const string & gpgExecutable_,
+             const string & mkisofsExecutable_) throw(Image::Exception)
+    : imageId(imageId_),
+      password(password_),
+      compression(compression_),
+      files(files_),
+      reject(reject_),
       diskspace(diskspace_),
-      allocatedMegabytes(0)
+      cdCapacity(cdCapacity_),
+      method(method_),
+      tarExecutable(tarExecutable_),
+      bzip2Executable(bzip2Executable_),
+      gpgExecutable(gpgExecutable_),
+      mkisofsExecutable(mkisofsExecutable_),
+      allocatedMegabytes(0),
+      imageMaxMegabytes(int(float(cdCapacity * CD_BLOCKSIZE)
+                            / float(MEGABYTE)) + 1),   // rounding up
+      imageMaxCdBlocks(cdCapacity),
+      estimatedIndexFileSize(0),
+      imageReady(false)
 {
-    char buffer[BUFFER_SIZE];
-    int targetFileSize = 0;
+    assert(files.empty() == false);
+    assert((compression >= 1) && (compression <= 9));
+
+    for (list<string>::const_iterator iter = files.begin();
+         iter != files.end();
+         ++iter) {
+        assert(!iter->empty());
+        assert((*iter)[0] == '/'); // absolute filenames required
+    }
+
+    /*
+     * If we are not allowed to use as much harddisk space as would fit on a cd,
+     * then we must create only partially filled cds.
+     */
+    if ((diskspace.getUsableMegabytes() * MEGABYTE)
+        < (cdCapacity * CD_BLOCKSIZE)) {
+        /* Number of files fitting on CD constrained by usable harddisk space */
+        imageMaxMegabytes = diskspace.getUsableMegabytes();
+        imageMaxCdBlocks = int(float(imageMaxMegabytes * MEGABYTE)
+                               / float(CD_BLOCKSIZE)); // rounding down
+    }
+
+    /*
+     * estimate the blocks needed for an index file:
+     */
+    for (list<string>::const_iterator iter = files.begin();
+         iter != files.end();
+         ++iter) {
+        estimatedIndexFileSize += iter->length() + 1;
+    }
+    estimatedIndexFileBlocks = (estimatedIndexFileSize / CD_BLOCKSIZE) + 1;
     
-    // allocatedMegabytes =
-    //     diskspace.allocate(cdCapacity*2048.0/(1024.0*1024.0)+1);
-
-    TarCreator * tarCreator = new TarCreator(tarExecutable, files);
-    Bzip2 * bz2Compressor = new Bzip2(bzip2Executable, 6, // compression rate
-                                      tarCreator->getStdoutPipeFd());
-    Gpg * gpgEncrypter = new Gpg(gpgExecutable, password, Gpg::ENCRYPT,
-                                 bz2Compressor->getStdoutPipeFd());
-
-    Gpg * gpgDecrypter = new Gpg(gpgExecutable, password, Gpg::DECRYPT);
-
-    // the decrypter's stdin may not be shared by child processes:
-    setCloseOnExecFlag(gpgDecrypter->getStdinPipeFd());
-    Bzip2 * bz2Inflator = new Bzip2(bzip2Executable, -1, // -1 means decompress
-                                    gpgDecrypter->getStdoutPipeFd());
-    TarLister * tarLister = new TarLister(tarExecutable,
-                                          bz2Inflator->getStdoutPipeFd());
-
-    int intermediateFd =
-        open("/tmp/kryptocd_inter_test.tar.bz2.gpg",
-             O_WRONLY|O_CREAT|O_TRUNC,
-             0644);
+    /*
+     * Calculate the maximum size permitted for the archive file:
+     */
+    {
+        /* calculate in 2 steps to prevent an overflow */
+        archiveFileMaxSize = (imageMaxCdBlocks - CD_BLOCKS_FOR_ISO_STRUCTURE
+                              - estimatedIndexFileBlocks);
+        archiveFileMaxSize *= CD_BLOCKSIZE;
+    }
+    
+    /*
+     * Reserve the needed harddisk space. It is possible that we do not receive
+     * all required space at once. Part of the space may be occupied by previous
+     * cd images that have still to be written to cd before they will be
+     * deleted. But we will start to assemble this image to the extent possible
+     * now, and then wait until more hard disk space is freed.
+     */
+    allocatedMegabytes = diskspace.allocate(imageMaxMegabytes);
 
     do {
-        int bytesLeft = cdCapacity - targetFileSize;
-        int bytesThisTime;
-
-        bytesThisTime = read(gpgEncrypter->getStdoutPipeFd(), buffer,
-                             BUFFER_SIZE);
-
-        if (bytesThisTime == 0) {
-            // The whole archive fits on one cd
-            break;
+        assembleImageData();
+        if (imageReady == false) {
+            rejectFirstFile();
+            if (files.empty()) {
+                /* we cannot create a cd: all files too big to fit */
+                throw Exception();
+            }
         }
+    } while (imageReady == false);
+    imageInfos.push_back(ImageInfo());
+    imageInfos.back().imageId = imageId;
+    imageInfos.back().files = thisTimeFileList;
+}
 
-        assert(bytesThisTime > 0);
-
-        targetFileSize += bytesThisTime;
-
-        if (targetFileSize >= cdCapacity) {
-            bytesThisTime -= targetFileSize - cdCapacity;
-            assert (bytesThisTime >= 0);
-        }
-
-        write(intermediateFd, buffer, bytesThisTime);
-        
-        while (bytesThisTime > 0) {
-            int bytesWritten;
-            
-            bytesWritten = write(gpgDecrypter->getStdinPipeFd(), buffer,
-                                 bytesThisTime);
-
-            assert(bytesWritten > 0);
-            bytesThisTime -= bytesWritten;
-        }
-    } while (targetFileSize < cdCapacity);
-    close(gpgDecrypter->getStdinPipeFd());
-    close(intermediateFd);
-    
-    // kill the archive creating processes
-    delete tarCreator;
-    tarCreator = 0;
-    delete bz2Compressor;
-    bz2Compressor = 0;
-    delete gpgEncrypter;
-    gpgEncrypter = 0;
-
-    // wait for the archive listing processes to finish
-    gpgDecrypter->wait();
-    delete gpgDecrypter;
-    gpgDecrypter = 0;
-    bz2Inflator->wait();
-    delete bz2Inflator;
-    bz2Inflator = 0;
-
-    list<string> newFileList;
-    if (targetFileSize < cdCapacity) {
-        newFileList = files;
-    } else {
-        newFileList = tarLister->getFileList();
-        // the last file is probably incomplete
-        if (!newFileList.empty()) {
-            newFileList.pop_back();
-        }
+void Image::rejectFirstFile(void) {
+    if ((imageReady == false) &&  !files.empty()) {
+        reject.push_back(files.front());
+        files.pop_front();
     }
+}        
 
-    for (std::list<std::string>::const_iterator i=newFileList.begin();
-         i != newFileList.end(); ++i) {
-        cout << "will include file: "<< *i << endl;
+void Image::assembleImageData(void) {
+    int timesFilesetReduced = 0;
+
+    thisTimeFileList = files;
+    do {
+        // create the archive, check if it fits on the cd, and if not, deduce
+        // what files would fit.
+        /*
+         * create a compressed, encrypted tar archive and learn what files
+         * would fit into a limited size archive:
+         */
+        ArchiveCreator * archiveCreator = new ArchiveCreator(tarExecutable,
+                                                             bzip2Executable,
+                                                             gpgExecutable,
+                                                             thisTimeFileList,
+                                                             compression,
+                                                             password);
+        /*
+         * prepare to list the contents of the compressed, encrypted, and then
+         * cutted to the permitted size archive:
+         */
+        ArchiveLister * archiveLister = new ArchiveLister(tarExecutable,
+                                                          bzip2Executable,
+                                                          gpgExecutable,
+                                                          password);
+
+        /*
+         * create the output file:
+         */
+        int outputFd =
+            open("/tmp/kryptocd_test.tar.bz2.gpg",
+                 O_WRONLY|O_CREAT|O_TRUNC,
+                 0600);
+        /*
+         * piping the compressed, encrypted tar file to the decryter and to the
+         * archive file manually, cutting after the cd capacity is reached:
+         */
+        IoPump archivePump(archiveCreator->getStdoutPipeFd(),
+                           archiveLister->getStdinPipeFd(), outputFd);
+        long long archiveFileSize = archivePump.pump(archiveFileMaxSize);
+
+        // kill the archive creating processes
+        delete archiveCreator;
+        archiveCreator = 0;
+
+        // close the file descriptors to which the archive was sent
+        close(outputFd);
+        archiveLister->closeStdinPipe();
+
+        thisTimeFileList.clear();
+        for (list<string>::const_iterator iter =
+                 archiveLister->getFileList().begin();
+             iter != archiveLister->getFileList().end();
+             ++iter) {
+            thisTimeFileList.push_back("/" + *iter);
+        }
+        delete archiveLister;
+        archiveLister = 0;
+
+        if (archiveFileSize < archiveFileMaxSize) {
+            // All files made it into the archive.
+            imageReady = true;
+        } else {
+            /* All files together do not fit on cd. */
+            if (timesFilesetReduced++ == 0) {
+                /*
+                 * That's ok and simple to deal with. Just examine what
+                 * files would have fitted onto this cd and try again with these
+                 * files only.
+                 */
+                if (!thisTimeFileList.empty()) {
+                    /* the last file in the list was incompletely stored */
+                    thisTimeFileList.pop_back();
+                }
+            } else {
+                /*
+                 * We have a problem here: We have already reduced the number of
+                 * files so that the ones we selected this time would have fit
+                 * on cd if they still had the same sizes as when we checked.
+                 * At least one file must have grown in the meantime.
+                 * Radically reduce the number of files to store in the archive!
+                 * Delete the second half of filenames.
+                 */
+                list<string>::iterator iter = thisTimeFileList.begin();
+                for (int i = 0; i < (thisTimeFileList.size() / 2); ++i, ++iter);
+                thisTimeFileList.erase(iter, thisTimeFileList.end());
+            }
+            if (thisTimeFileList.empty()) {
+                /* the first file in the list is too large to fit on a cd */
+                return; // reject that file
+            }
+        }
+    } while ((imageReady == false) && !thisTimeFileList.empty());
+    if (imageReady) {
+        // remove the stored files from the list:
+        list<string>::iterator iter = files.begin();
+        for (int i = 0; i < thisTimeFileList.size(); ++i, ++iter);
+        files.erase(files.begin(), iter);
     }
+}
 
-    
-    
-    delete tarLister;
-    tarLister = 0;
-
-    /* The archive file */
-    int outputFd =
-        open("/tmp/kryptocd_test.tar.bz2.gpg",
-             O_WRONLY|O_CREAT|O_TRUNC,
-             0644);
-    
-    tarCreator = new TarCreator(tarExecutable, newFileList);
-    bz2Compressor = new Bzip2(bzip2Executable, 6, // compression rate
-                              tarCreator->getStdoutPipeFd());
-    gpgEncrypter = new Gpg(gpgExecutable, password, Gpg::ENCRYPT,
-                           bz2Compressor->getStdoutPipeFd(),
-                           outputFd);
-
-    tarCreator->wait();
-    delete tarCreator;
-    tarCreator = 0;
-    bz2Compressor->wait();
-    delete bz2Compressor;
-    bz2Compressor = 0;
-    gpgEncrypter->wait();    
-    delete gpgEncrypter;
-    gpgEncrypter = 0;
-}    
--- kryptocd/source/kernel/image.hh
+++ kryptocd/source/kernel/image.hh
@@ -1,7 +1,7 @@
 /*
  * image.hh: class Image header file
  * 
- * $Id: image.hh,v 1.2 2001/04/28 11:31:39 t-peters Exp $
+ * $Id: image.hh,v 1.3 2001/05/02 21:47:07 t-peters Exp $
  *
  * This file is part of KryptoCD
  * (c) 2001 Tobias Peters
@@ -26,26 +26,143 @@
 #define IMAGE_HH
 
 #include <list>
+#include <iostream>
 #include "diskspace.hh"
+#include "image_info.hh"
+
+#ifndef MEGABYTE
+#define MEGABYTE (1024 * 1024)
+#endif
+
+#ifndef CD_BLOCKSIZE
+#define CD_BLOCKSIZE 2048
+#endif
+
+#ifndef CD_BLOCKS_FOR_ISO_STRUCTURE
+#define CD_BLOCKS_FOR_ISO_STRUCTURE 40 // 31 would probably be enough, but we
+#endif                                 // must be sure.
 
 namespace KryptoCD {
+    /**
+     * Class Image assembles files for the burning process
+     *
+     * @author  Tobias Peters
+     * @version $Revision: 1.3 $ $Date: 2001/05/02 21:47:07 $
+     */
     class Image {
     public:
+        class Exception{};
         enum Method {SINGLE_TAR_FILE, INDEX_FILE};
-        Image(const std::string & image_id,
+
+        /**
+         * The Image constructor's task is to assemble all files needed
+         * by the cdimage, so that burning a cd with mkisofs used on-the-fly
+         * succeeds.
+         *
+         * @param imageId    used as the volume id of the cd
+         * @param password   the password used for all gpg -c encryption
+         * @param compression the compression rate used for the bzip2
+         *                   compression. *Must* be between 1 and 9.
+         * @param files      a list of files still needing to be archived.
+         *                   Files archived inside this image are removed from
+         *                   this list
+         * @param reject     the image constructor will save names of files that
+         *                   cannot be archived (usually due to their size) in
+         *                   this list
+         * @param diskspace  a reference to the harddisk space manager
+         * @param cdCapacity the number of usable blocks on the target cd. This
+         *                   is the number reported by cdrecord -atip in the
+         *                   line "ATIP start of lead out:". A block on cd has
+         *                   room for 2048 bytes.
+         * @param method     one of the supported archive methods: either
+         *                   Image::SINGLE_TAR_FILE or Image::INDEX_FILE
+         * @param tarExecutable     the location of the GNU tar executable file
+         * @param bzip2Executable   the location of the bzip2 executable file
+         * @param gpgExecutable     the location of the GNU privacy guard
+         *                          executable file
+         * @param mkisofsExecutable the location of the mkisofs executable file
+         * @param logger      A stream for log messages.
+         */
+        Image(const std::string & imageId,
               const std::string & password,
-              const std::list<std::string> & files,
+              int compression,
+              std::list<std::string> & files,
+              std::list<std::string> & reject,
+              std::list<ImageInfo> & imageInfos,
               Diskspace & diskspace,
               int cdCapacity,
               Method method,
               const std::string & tarExecutable,
               const std::string & bzip2Executable,
               const std::string & gpgExecutable,
-              const std::string & mkisofsExecutable);
+              const std::string & mkisofsExecutable)
+            throw(Exception);
     private:
-        string image_id;
+        std::string imageId;
+        std::string password;
+        int compression;
+        std::list<std::string> & files;
+        std::list<std::string> & reject;
         Diskspace & diskspace;
+        int cdCapacity;
+        Method method;
+        std::string tarExecutable;
+        std::string bzip2Executable;
+        std::string gpgExecutable;
+        std::string mkisofsExecutable;
+
         int allocatedMegabytes;
+        
+        /**
+         * The number of megabytes to allocate on harddisk for this cd:
+         */
+        int imageMaxMegabytes;
+
+        /**
+         * The image will not occupy more cd blocks on cd than this number:
+         */
+        int imageMaxCdBlocks;
+
+        /**
+         * An upper limit estimation for the size (in bytes) of an encrypted
+         * file containing all names of files stored on this cd.
+         */
+        int estimatedIndexFileSize;
+
+        /**
+         * An upper limit estimation for the size in cd blocks of an encrypted
+         * file containing all names of files stored on this cd.
+         */
+        int estimatedIndexFileBlocks;
+
+        /**
+         * The maximum allowed size (in bytes) for the achive file
+         */
+        long long archiveFileMaxSize;
+
+        /**
+         * The list of files to be stored on this cd
+         */
+        std::list<std::string> thisTimeFileList;
+
+        /**
+         * A flag indicating that the image is ready for burning
+         */
+        bool imageReady;
+
+        /**
+         * try to create the files needed for
+         */
+        void assembleImageData(void);
+        void rejectFirstFile(void);
+        
+        std::list<std::string>
+        getSingleTarFileList(const string & password,
+                             int compression,
+                             const list<string> & files, int tarFileCDBlocks,
+                             const string & tarExecutable,
+                             const string & bzip2Executable,
+                             const string & gpgExecutable);
     };
 }
         
