636a9a67d2a4cdddf23b415625ebd4443bbda63b
t-peters <t-peters>
2001-05-19 21:53:29 +0000

    Bzip2::Bzip2                     replaced plain file descriptors with classes                                  Source and Sink Bzip2::argumentList              simplified Bzip2::childToParentFdMap        method removed

--- kryptocd/source/kernel/bzip2.cpp
+++ kryptocd/source/kernel/bzip2.cpp
@@ -1,7 +1,7 @@
 /*
  * bzip2.cpp: Implementation of class Bzip2
  * 
- * $Id: bzip2.cpp,v 1.1 2001/04/23 21:21:54 t-peters Exp $
+ * $Id: bzip2.cpp,v 1.2 2001/05/19 21:53:29 t-peters Exp $
  *
  * This file is part of KryptoCD
  * (c) 2001 Tobias Peters
@@ -27,51 +27,29 @@
 #include <unistd.h>
 
 using KryptoCD::Bzip2;
+using KryptoCD::Sink;
+using KryptoCD::Source;
+using KryptoCD::ChildFilter;
 using std::string;
-using std::list;
-using std::map;
 using std::vector;
 
 Bzip2::Bzip2(const string & bzip2Executable, int compression,
-             int bzip2StdinFd = -1, int bzip2StdoutFd = -1)
-    : Childprocess(bzip2Executable,
-                   Bzip2::argumentList(bzip2Executable, compression),
-                   Bzip2::childToParentFdMap(bzip2StdinFd,
-                                             bzip2StdoutFd))
+             Source & source, Sink & sink)  throw (ChildFilter::Exception)
+    : ChildFilter(bzip2Executable,
+                  Bzip2::argumentList(bzip2Executable, compression),
+                  source, sink)
 {}
 
 vector<string> Bzip2::argumentList(const string & bz2Executable,
                                    int compression)               {
     vector<string> argumentList;
-    char compressionArg [3] = "-9";
-
-    /*
-     * Replace the default digit '9' in the compression level argument with the
-     * right digit. Its value is given in compression.
-     */
-    if ((compression > 0) && (compression < 10)) {
-        compressionArg[1] = '0' + compression;
-    }
 
     argumentList.push_back(bz2Executable);
     argumentList.push_back("--stdout");
     if ((compression > 0) && (compression < 10)) {
-        argumentList.push_back(compressionArg);
+        argumentList.push_back(string("-") + char('0' + compression));
     } else {
         argumentList.push_back("--decompress");
     }
     return argumentList;
-}
-
-map<int,int> Bzip2::childToParentFdMap(int bzip2StdinFd,
-                                       int bzip2StdoutFd) {
-    map<int,int> childToParentFdMap;
-
-    if (bzip2StdinFd != -1) {
-        childToParentFdMap[0]=bzip2StdinFd;
-    }
-    if (bzip2StdoutFd != -1) {
-        childToParentFdMap[1]=bzip2StdoutFd;
-    }
-    return childToParentFdMap;
 }
