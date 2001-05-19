2ebd1b2b1163054eda8aca4c56e29e0f83164783
t-peters <t-peters>
2001-05-19 21:56:35 +0000

    unsignedToString                 new static function main                             adapted to new Image interface;                                  distributing the archive over several images;                                  deleting created files again after [enter] key

--- kryptocd/source/kernel/test_image.cpp
+++ kryptocd/source/kernel/test_image.cpp
@@ -1,6 +1,6 @@
 /* test_image.cpp: test program for class Image
  *
- * $Id: test_image.cpp,v 1.2 2001/05/02 21:47:58 t-peters Exp $
+ * $Id: test_image.cpp,v 1.3 2001/05/19 21:56:35 t-peters Exp $
  *
  * This file is part of KryptoCD
  * (c) 2001 Tobias Peters
@@ -26,6 +26,22 @@
 #include "image.hh"
 
 /**
+ * return a string representation of an unsigned integer:
+ */
+static string unsignedToString(unsigned i) {
+    string s;
+    
+    if (i == 0) {
+        return "0";
+    }
+    while (i > 0) {
+        s.insert(s.begin(), char('0' + i % 10));
+        i /= 10;
+    }
+    return s;
+}
+
+/**
  * This is a test program for class Image. As its first command line argument,
  * it expects the number of usable blocks on a cd, as reported by
  * "cdrecord -atip" in the line starting with "  ATIP start of lead out".
@@ -40,37 +56,74 @@ int main(int argc, char ** argv) {
     int capacity = atoi(argv[1]);
 
     /* a list of files to put into the archive */
-    std::list<std::string> files, reject;
+    std::list<std::string> files, rejectedBigFiles;
+    std::list<std::string> rejectedForbiddenFiles, rejectedBadNamedFiles;
     
     for (int i = 2; i < argc; ++i) {
         files.push_back(argv[i]);
     }
-    ostream * logger[5] = {&cerr,&cerr,&cerr,&cerr,&cerr};
 
     std::list<KryptoCD::ImageInfo> imageInfos;
     KryptoCD::Diskspace ds("/tmp", 700);
-    delete new KryptoCD::Image ("image_id",
-                                "some_password",
-                                6, // compression level
-                                files,
-                                reject,
-                                imageInfos,
-                                ds,
-                                capacity,
-                                KryptoCD::Image::SINGLE_TAR_FILE,
-                                "/bin/tar",
-                                "/usr/bin/bzip2",
-                                "/usr/bin/gpg",
-                                "/usr/bin/mkisofs"
-                                );
-    if (reject.empty()) {
-        cout << "No files were rejected because of their current size." << endl;
+
+    unsigned i = 0;
+    list<KryptoCD::Image*> images;
+    while (!files.empty()) {
+        ++i;
+        images.push_back(new KryptoCD::Image ("image_id" + unsignedToString(i),
+                                              "some_password",
+                                              6, // compression level
+                                              files,
+                                              rejectedBigFiles,
+                                              rejectedForbiddenFiles,
+                                              rejectedBadNamedFiles,
+                                              imageInfos,
+                                              ds,
+                                              capacity,
+                                              KryptoCD::Image::SINGLE_TAR_FILE,
+                                              "/bin/tar",
+                                              "/usr/bin/bzip2",
+                                              "/usr/bin/gpg",
+                                              "/usr/bin/mkisofs"
+                                              ));
+    }
+    /* Create a report */
+    if (rejectedBigFiles.empty()) {
+        cout << "No files were rejected because of their current size."
+             << endl;
     } else {
         cout << "These files have been too big to be saved: ";
-        for (std::list<std::string>::const_iterator iter = reject.begin();
-             iter != reject.end();
+        for (std::list<std::string>::const_iterator iter =
+                 rejectedBigFiles.begin();
+             iter != rejectedBigFiles.end();
              ++iter) {
-            cout << *iter << "\\";
+            cout << *iter << "  \\  ";
+        }
+        cout << endl;
+    }
+    if (rejectedForbiddenFiles.empty()) {
+        cout << "No files were rejected because of their permissions."
+             << endl;
+    } else {
+        cout << "These files have been left out due to their permissions: ";
+        for (std::list<std::string>::const_iterator iter =
+                 rejectedForbiddenFiles.begin();
+             iter != rejectedForbiddenFiles.end();
+             ++iter) {
+            cout << *iter << "  \\  ";
+        }
+        cout << endl;
+    }
+    if (rejectedBadNamedFiles.empty()) {
+        cout << "No files were rejected because of their name."
+             << endl;
+    } else {
+        cout << "These files have been left out because of their names: ";
+        for (std::list<std::string>::const_iterator iter =
+                 rejectedBadNamedFiles.begin();
+             iter != rejectedBadNamedFiles.end();
+             ++iter) {
+            cout << *iter << "  \\  ";
         }
         cout << endl;
     }
@@ -78,14 +131,21 @@ int main(int argc, char ** argv) {
         cout << "No files have been saved." << endl;
     }
     else {
-        cout << "These files have been saved: ";
-        for (std::list<std::string>::const_iterator iter =
-                 imageInfos.back().files.begin();
-             iter != imageInfos.back().files.end();
-             ++iter) {
-            cout << *iter << "\\";
+        cout << "These files have been saved:\n";
+        int i;
+        std::list<KryptoCD::ImageInfo>::iterator infoIterator;
+        for (i = 1,  infoIterator = imageInfos.begin();
+             infoIterator != imageInfos.end();
+             ++i, ++infoIterator) {
+            cout << "####### On cd number " << i << ": ";
+            for (std::list<std::string>::const_iterator iter =
+                     infoIterator->files.begin();
+                 iter != infoIterator->files.end();
+                 ++iter) {
+                cout << *iter << "  \\  ";
+            }
+            cout << endl;
         }
-        cout << endl;
     }
     if (files.empty()) {
         cout << "No files have been queued for a next cd." << endl;
@@ -94,8 +154,18 @@ int main(int argc, char ** argv) {
         for (std::list<std::string>::const_iterator iter = files.begin();
              iter != files.end();
              ++iter) {
-            cout << *iter << "\\";
+            cout << *iter << "  \\  ";
         }
         cout << endl;
+    }
+    cout << "You may now now examine the created images (they are in /tmp)."
+         << endl
+         << "Press <return> when you are finished, they will then be deleted "
+         << "again." << endl;
+    string line;
+    getline(cin,line);
+    while(!images.empty()) {
+        delete images.front();
+        images.pop_front();
     }
 }
