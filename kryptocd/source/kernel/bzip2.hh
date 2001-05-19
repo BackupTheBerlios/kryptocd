c151f67005eddfae5f3ef1750f4305326a421601
t-peters <t-peters>
2001-05-19 21:53:46 +0000

    class Bzip2                      now inherits from ChildFilter instead of                                  Childprocess Bzip2::Bzip2                     replaced plain file descriptors with classes                                  Source and Sink.                                  now throws Exception Bzip2::childToParentFdMap        static method removed

--- kryptocd/source/kernel/bzip2.hh
+++ kryptocd/source/kernel/bzip2.hh
@@ -1,7 +1,7 @@
 /*
  * bzip2.hh: class Bzip2 header file
  * 
- * $Id: bzip2.hh,v 1.1 2001/04/23 21:21:54 t-peters Exp $
+ * $Id: bzip2.hh,v 1.2 2001/05/19 21:53:46 t-peters Exp $
  *
  * This file is part of KryptoCD
  * (c) 2001 Tobias Peters
@@ -26,53 +26,46 @@
 #ifndef BZIP2_HH
 #define BZIP2_HH
 
-#include "childprocess.hh"
+#include "child_filter.hh"
 #include <list>
 
 namespace KryptoCD {
     /**
-     * Bzip2 compresses or decompresses data from one file descriptor to another
-     * one, using bzip2.
+     * Bzip2 compresses or decompresses data from one file descriptor to
+     * another one, using bzip2.
      *
      * @author Tobias Peters
-     * @version $Revision: 1.1 $ $Date: 2001/04/23 21:21:54 $
+     * @version $Revision: 1.2 $ $Date: 2001/05/19 21:53:46 $
      */
-    class Bzip2 : public Childprocess {
+    class Bzip2 : public ChildFilter {
     public:
         /**
-         * By intanciating a Bz2 object, bzip2 is invoked as
-         * a child process. It reads data from the file descriptor
-         * bzip2StdinFd, compresses it and writes the compressed
-         * output to the file descriptor  bzip2StdoutFd .
+         * By intanciating a Bzip2 object, bzip2 is invoked as
+         * a child process. It reads data from the given source,
+         * compresses (or decompresses) it and writes the (de)compressed
+         * output to the given sink.
          *
          * @param bzip2Executable  the full path to the bzip2 executable file
          * @param compression      the level of compression that bzip2 will use
          *                         when compressing data. Valid compression
-         *                         levels are 1,2,...,9. If the number given here
-         *                         is outside this interval (suggestion: -1),
-         *                         then bzip2 will decompress its stdin.
-         * @param bzip2StdinFd     the file descriptor from which bzip2 will read
-         *                         the data to compress or decompress. If left to
-         *                         the default, this object will create a new
-         *                         Pipe, and make the datasink part of this pipe
-         *                         accessible via Childprocess::getStdinPipeFd.
-         *                         In either case, the file descriptor that bzip2
-         *                         uses to read data from will be closed within
+         *                         levels are 1,2,...,9. If the number given
+         *                         here is outside this interval (suggestion:
+         *                         -1), then bzip2 will decompress its stdin.
+         * @param source           the source from which bzip2 will read
+         *                         the data to compress or decompress.
+         *                         This source will be closed within
          *                         this process.
-         * @param bzip2StdoutFd    the file descriptor to which bzip2 will write
-         *                         the compressed or decompressed data. If left
-         *                         to the default, this object will create a new
-         *                         Pipe, and make the datasource part of this
-         *                         pipe accessible via
-         *                         Childprocess::getStdoutPipeFd. In either case,
-         *                         the file descriptor that bzip2 uses
-         *                         to write data to will be closed within this
+         * @param sink             The destination of
+         *                         the compressed or decompressed data.
+         *                         This sink will be closed within this
          *                         process.
+         * @throw Childprocess::Exception                   thrown
+         *                         when the fork system call fails
          */
         Bzip2(const std::string & bzip2Executable,
               int compression,
-              int bzip2StdinFd = -1,
-              int bzip2StdoutFd = -1);
+              Source & source,
+              Sink & sink) throw (Childprocess::Exception);
 
     private:
         /**
@@ -89,22 +82,6 @@ namespace KryptoCD {
          */
         static std::vector<std::string>
         argumentList(const std::string & bz2Executable, int compression);
-
-        /**
-         * childToParentFdMap creates a map<int,int> which is
-         * needed to initialize the parent class Childprocess:
-         * It decides if the Bzip2 object wants to use existing file descriptors
-         * for bzip's stdin and stdout and creates an appropriate file descriptor
-         * map.
-         *
-         * @param bzip2StdinFd   the file descriptor from which bzip2 will read
-         *                       data
-         * @param bzip2StdoutFd  the file descriptor to which bzip2 will write
-         *                       data
-         * @see Childprocess
-         */
-        static map<int,int> childToParentFdMap(int bzip2StdinFd,
-                                               int bzip2StdoutFd);
     };
 }
 
